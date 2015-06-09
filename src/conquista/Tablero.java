6fc4ba1742a5af67d4e36ce95cb2d6731408e28c
vchicor0d <vchicorod@gmail.com>
2015-06-09 12:36:41 +0200

    Víctor Chico - Cambio en la definición del método validarMovimiento

--- src/conquista/Tablero.java
+++ src/conquista/Tablero.java
@@ -19,10 +19,20 @@ public class Tablero {
 	}
 	
 	public int[][] conversorCasilla(String casilla){
+		bora bora
 		return null;
 	}
 	
-	public boolean validarMovimiento(String cOrigen, String cDestino){
+	/**
+	 * Comprueba si la acción de movimiento es válida.
+	 * 
+	 * @param cOrigen Casilla origen de la acción
+	 * @param cDestino Casilla destino de la acción (Puede ser nula para defensa)
+	 * @param tipo Tipo de acción a ejecutar 'a' para ataque y 'd' para defensa
+	 * @param idj ID del jugador
+	 * @return true si la acción es válida y false si no lo es
+	 */
+	public boolean validarMovimiento(String cOrigen, String cDestino, char tipo, char idj){
 		return false;
 	}
 }
