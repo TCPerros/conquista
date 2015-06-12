8d9bd060698a9e4d401d9b8f144e9fcec7c279c9
vchicor0d <vchicorod@gmail.com>
2015-06-12 13:09:11 +0200

    Víctor - Subida de métodos esqueleto del controlador

--- src/conquista/Controlador.java
+++ src/conquista/Controlador.java
@@ -2,4 +2,25 @@ package conquista;
 
 public class Controlador {
 
+	public Controlador(){
+		super();
+	}
+	
+	public void principal(){
+		
+	}
+	
+	private boolean menuInicial(){
+		
+		return true;
+	}
+	
+	private byte menuAccion() {
+		return 0;
+	}
+	
+	private boolean resolucion(){
+		
+		return false;
+	}
 }
