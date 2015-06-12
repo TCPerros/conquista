45fb98f35822cc3b52fce7b8a4f8359642d0b0c9
vchicor0d <vchicorod@gmail.com>
2015-06-12 13:56:01 +0200

    Víctor - Inicio de método controlador

--- src/conquista/Controlador.java
+++ src/conquista/Controlador.java
@@ -19,8 +19,29 @@ public class Controlador {
 		return 0;
 	}
 	
-	private boolean resolucion(Jugador j1, Jugador J2, Tablero t){
-		
+	private boolean resolucion(Jugador j1, Jugador j2, Tablero t){
+		Casilla[][] nuevoTablero = t.getCasillas();
+		int resCasilla, movj1, movj2;
+		char eCasilla;
+		int[][] resolucion = new int[nuevoTablero.length][nuevoTablero[0].length];
+		for (int i = 0; i < resolucion.length; i++){
+			for (int j = 0; j < resolucion[0].length; j++){
+				resCasilla = nuevoTablero[i][j].getResistencia();
+				eCasilla = nuevoTablero[i][j].getEquipo();
+				movj1 = j1.getMovimientos()[i][j];
+				movj2 = j2.getMovimientos()[i][j];
+				if (movj1 < 0){
+					if (movj2 < 0){
+						resCasilla = (resCasilla+movj1+movj2 < 0)? 0 : resCasilla+movj1+movj2;
+						if (resCasilla == 0) {
+							if (movj1>movj2) eCasilla = j2.getId();
+							else if (movj2>movj1) eCasilla = j1.getId();
+						}
+					}
+				}
+				//resolucion[i][j] = j1.getMovimientos()[i][j] + j2.getMovimientos()[i][j];
+			}
+		}
 		return false;
 	}
 }
